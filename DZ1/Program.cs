c1d018479a17338dd6145579e846edbea4e54312
Name <limpamposha@mail.ru>
2022-11-24 21:42:49 +0300

    первая задача из дз 1

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число:");
+
+           int a = Convert.ToInt32(Console.ReadLine());
+
+           Console.WriteLine("Введите второе число:");
+
+           int b = Convert.ToInt32(Console.ReadLine());
+
+           if (a < b)
+           {
+                Console.WriteLine($"{a} меньше {b}");
+           }
+           else if (a > b)
+           {
+                Console.WriteLine($"{a} больше {b}");
+           }
+           else
+           {
+                Console.WriteLine($"{a} равно {b}");
+           }
+
+           
+           
+           
