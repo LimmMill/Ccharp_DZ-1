352096dcad0c975123c939492c0fc72e1f6eeb07
Name <limpamposha@mail.ru>
2022-11-27 20:50:22 +0300

    DZ1 zadacha 8

---
+++ DZ1_8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введи число");
+int num = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+while (index <= num)
+{
+   if (index % 2 == 0)
+   {
+    Console.Write( index +", " );
+    index++;
+   }
+    else
+    {
+      index++;
+    }
+}
